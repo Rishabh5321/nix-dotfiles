e81639c3ea57d22bcfe67268f2fdd2e4684ecbdf
Rishabh5321 <Rishabh5321@users.noreply.github.com>
2024-07-20 03:59:07 +0000

    Auto lint/format

--- common/system/pkgs_unstable.nix
+++ common/system/pkgs_unstable.nix
@@ -7,24 +7,24 @@
   options,
   ...
 }: {
-  environment.systemPackages =
-  let
-  cursor = pkgs-unstable.callPackage ../../pkgs/cursor.nix { };
-  in with pkgs-unstable; [
-    github-desktop
-    heroic
-    cursor
-    #inputs.nix-software-center.packages.${system}.nix-software-center
-    inputs.alejandra.defaultPackage.${system}
-    lutris
-    protonup-qt
-    #sddm-astronaut
-    rquickshare
-    zed-editor
-    wezterm
-    vscode
-    #plex
-  ];
+  environment.systemPackages = let
+    cursor = pkgs-unstable.callPackage ../../pkgs/cursor.nix {};
+  in
+    with pkgs-unstable; [
+      github-desktop
+      heroic
+      cursor
+      #inputs.nix-software-center.packages.${system}.nix-software-center
+      inputs.alejandra.defaultPackage.${system}
+      lutris
+      protonup-qt
+      #sddm-astronaut
+      rquickshare
+      zed-editor
+      wezterm
+      vscode
+      #plex
+    ];
   #services.plex.enable = true;
   #services.hypridle.enable = true;
 }
