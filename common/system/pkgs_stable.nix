2da6d68306f086f19cba9344fa83828ab99dfea2
Rishabh5321 <rishabh98818@outlook.com>
2024-07-23 18:35:12 +0530

    updates

--- common/system/pkgs_stable.nix
+++ common/system/pkgs_stable.nix
@@ -12,75 +12,85 @@
     #  cursor = pkgs.callPackage ./pkgs/cursor.nix { };
     #in
     with pkgs; [
+      # Terminal and Shell
       alacritty
+      figlet
+      fzf
+      micro
+      nh
+      ranger
+      vim
+      zoxide
+      zinit
+      zsh
+      zsh-powerlevel10k
+
+      # Internet and Communication
+      brave
+      firefox
+      jellyfin-media-player
+      kdePackages.kdeconnect-kde
+      qbittorrent
+      resilio-sync
+      telegram-desktop
+      wget
+
+      # System Tools
       android-tools
       bitwarden-desktop
-      bottles
-      brave
-      btop
-      cava
-      #cursor
       cliphist
       distrobox
       eza
-      filezilla
       fast-cli
       fastfetch
       ffmpeg
-      firefox
-      #floorp
-      figlet
-      fzf
       gh
       hyprlock
       hyprshot
       gnome.gnome-disk-utility
       gnome.gnome-system-monitor
       gnome.nautilus
-      #google-chrome
-      jellyfin-media-player
-      #inputs.wezterm.packages.${pkgs.system}.default
-      kdePackages.kdeconnect-kde
       kodi-wayland
       lazycli
       lazydocker
       libreoffice
       nixos-generators
-      micro
-      nh
       onlyoffice-bin_latest
-      pfetch-rs
-      peaclock
-      plex-media-player
       polkit_gnome
-      #prismlauncher
-      qbittorrent
-      ranger
-      resilio-sync
-      spicetify-cli
-      spotdl
       speedtest-cli
-      #spicetify-cli
-      stremio
-      telegram-desktop
       tldr
-      uwufetch
-      vlc
       wayland-pipewire-idle-inhibit
-      wget
       wlroots
       xfce.xfce4-pulseaudio-plugin
-      vim
-      zoxide
-      zinit
-      zsh
-      zsh-powerlevel10k
       unetbootin
       ventoy
 
-      #server
-      #plex
+      # Multimedia
+      cava
+      filezilla
+      plex-media-player
+      stremio
+      vlc
+      peaclock
+      spicetify-cli
+      spotdl
 
+      # Development Tools
+      bottles
+      pfetch-rs
+      uwufetch
+
+      # Miscellaneous
+      btop
+      #floorp
+      #google-chrome
+      #prismlauncher
+      #spicetify-cli
+
+      # Unsorted/Commented
+      #cursor
+      #inputs.wezterm.packages.${pkgs.system}.default
+      #plex
       #where-is-my-sddm-theme
       #pkgs.libsForQt5.qt5.qtgraphicaleffects
     ];
