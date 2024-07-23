9ea8daea1ebfcb4f5604892e55402307a25edf3f
Rishabh5321 <Rishabh5321@users.noreply.github.com>
2024-07-23 05:33:53 +0000

    Auto lint/format

--- hosts/dell/docker-compose.nix
+++ hosts/dell/docker-compose.nix
@@ -1,11 +1,8 @@
-{ pkgs, ... }:
-
-let
+{pkgs, ...}: let
   compose = pkgs.dockerTools.servicesFromCompose {
     name = "example-compose";
     composeFile = ../../docker/docker-compose.yaml;
   };
-in
-{
+in {
   services.docker.containers = compose.services;
 }
