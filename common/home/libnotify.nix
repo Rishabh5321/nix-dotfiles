8b7c23b6457c8c627ed830b3532e20a587a8a835
Rishabh5321 <rishabh98818@outlook.com>
2024-07-24 19:59:42 +0530

    changes

--- common/home/libnotify.nix
+++ common/home/libnotify.nix
@@ -25,7 +25,7 @@
         progress_bar = true;
         transparency = 0;
         text_icon_padding = 0;
-        separator_color = "frame";
+        #separator_color = "frame";
         sort = "yes";
         idle_threshold = 120;
         line_height = 0;
@@ -37,17 +37,17 @@
         sticky_history = "yes";
         history_length = 20;
         always_run_script = true;
-        corner_radius = config.var.theme.rounding;
+        #corner_radius = config.var.theme.rounding;
         follow = "mouse";
-        font = config.var.theme.font;
+        #font = config.var.theme.font;
         format = "<b>%s</b>\\n%b";
-        progress_bar_corner_radius = config.var.theme.rounding - 10;
+        #rogress_bar_corner_radius = config.var.theme.rounding - 10;
         #format = ''
         #  <span foreground='#${config.stylix.base16Scheme.accent}'><b>%s %p</b></span>
         #  %b'';
-        frame_color = "#" + config.stylix.base16Scheme.base00;
-        highlight = "#" + config.stylix.base16Scheme.base01;
-        foreground = "#" + config.stylix.base16Scheme.base04;
+        # frame_color = "#" + config.stylix.base16Scheme.base00;
+        # highlight = "#" + config.stylix.base16Scheme.base01;
+        # foreground = "#" + config.stylix.base16Scheme.base04;
         frame_width = 1;
         offset = "0x10";
         horizontal_padding = 10;
@@ -69,18 +69,18 @@
 
       fullscreen_delay_everything = {fullscreen = "delay";};
 
-      urgency_critical = {
-        background = "#" + config.stylix.base16Scheme.base08;
-        foreground = "#" + config.stylix.base16Scheme.base0C;
-      };
-      urgency_low = {
-        background = "#" + config.stylix.base16Scheme.base0D;
-        foreground = "#" + config.stylix.base16Scheme.base0E;
-      };
-      urgency_normal = {
-        background = "#" + config.stylix.base16Scheme.base00;
-        foreground = "#" + config.stylix.base16Scheme.base0F;
-      };
+      # urgency_critical = {
+      #   background = "#" + config.stylix.base16Scheme.base08;
+      #   foreground = "#" + config.stylix.base16Scheme.base0C;
+      # };
+      # urgency_low = {
+      #   background = "#" + config.stylix.base16Scheme.base0D;
+      #   foreground = "#" + config.stylix.base16Scheme.base0E;
+      # };
+      # urgency_normal = {
+      #   background = "#" + config.stylix.base16Scheme.base00;
+      #   foreground = "#" + config.stylix.base16Scheme.base0F;
+      # };
     };
   };
 }
--- common/home/packages.nix
+++ common/home/packages.nix
@@ -13,5 +13,23 @@ in {
       inherit pkgs;
       inherit wallpaperDir;
     })
+    hyprshot
+    hyprpicker
+    # hyprcursor
+    xdg-desktop-portal-hyprland
+    wlr-randr
+    wl-clipboard
+    brightnessctl
+    gnome-themes-extra
+    wlsunset
+    xwayland
+    xdg-desktop-portal-gtk
+    wlroots
+    qt5ct
+    libva
+    dconf
+    wayland-utils
+    wayland-protocols
+    meson
   ];
 }
