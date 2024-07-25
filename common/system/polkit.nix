36db3f9df5db02dc9444d98b4f4386c8b67505ec
Rishabh5321 <Rishabh5321@users.noreply.github.com>
2024-07-25 05:33:42 +0000

    Auto lint/format

--- common/system/polkit.nix
+++ common/system/polkit.nix
@@ -3,28 +3,28 @@
   pkgs,
   ...
 }: {
-# Security / Polkit
-security.rtkit.enable = true;
-security.polkit.enable = true;
-security.polkit.extraConfig = ''
-  polkit.addRule(function(action, subject) {
-    if (
-      subject.isInGroup("users")
-        && (
-          action.id == "org.freedesktop.login1.reboot" ||
-          action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
-          action.id == "org.freedesktop.login1.power-off" ||
-          action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+  # Security / Polkit
+  security.rtkit.enable = true;
+  security.polkit.enable = true;
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (
+        subject.isInGroup("users")
+          && (
+            action.id == "org.freedesktop.login1.reboot" ||
+            action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+            action.id == "org.freedesktop.login1.power-off" ||
+            action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+          )
         )
-      )
-    {
-      return polkit.Result.YES;
-    }
-  })
-'';
-security.pam.services.swaylock = {
-  text = ''
-    auth include login
+      {
+        return polkit.Result.YES;
+      }
+    })
   '';
-};
+  security.pam.services.swaylock = {
+    text = ''
+      auth include login
+    '';
+  };
 }
