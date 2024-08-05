dd45fe6ee6af455d6679e73c88b687b310ea950c
Rishabh5321 <rishabh98818@outlook.com>
2024-08-05 15:32:04 +0530

    update to docs

--- nixpkgs.nix
+++
@@ -1,8 +0,0 @@
-# Arbitrary known-good revision for default use.
-let
-  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-in
-  import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${lock.nodes.nixpkgs.locked.rev}.tar.gz";
-    sha256 = lock.nodes.nixpkgs.locked.narHash;
-  })
