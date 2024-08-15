17b4d7fbafc395b7fe76f86ce1254c41680ba645
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2024-08-15 17:55:32 +0530

    update wallselector

--- pkgs/wall-selector.nix
+++ pkgs/wall-selector.nix
@@ -4,9 +4,14 @@
   ...
 }:
 pkgs.writeShellScriptBin "wallSelector" ''
-  chosen=$(ls ${wallpaperDir} | grep "wall*" | ${pkgs.rofi-wayland}/bin/rofi -dmenu -p "Select a wallpaper")
+  # Find all wallpapers in the directory that match the pattern "wall*"
+  chosen=$(find ${wallpaperDir} -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) -print0 | \
+           xargs -0 -I{} basename "{}" | \
+           ${pkgs.rofi-wayland}/bin/rofi -dmenu -p "Select a wallpaper" -config ~/.config/rofi/config-wallpaper.rasi)
 
+  # Exit if no wallpaper was selected
   [ -z "$chosen" ] && exit;
 
-  ${pkgs.swww}/bin/swww img ${wallpaperDir}/$chosen
+  # Set the selected wallpaper using swww
+  ${pkgs.swww}/bin/swww img ${wallpaperDir}/"$chosen"
 ''
