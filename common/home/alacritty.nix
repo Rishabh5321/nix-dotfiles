7a6ba5a4fe7ee837b83a15fe82f8c994a8740b2b
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2024-08-22 17:25:44 +0530

    fastfetch update

--- common/home/alacritty.nix
+++ common/home/alacritty.nix
@@ -1,25 +1,34 @@
-{lib, ...}: let
+{ lib, ... }:
+let
   inherit (import ../system/variables.nix) alacritty;
 in
-  lib.mkIf alacritty {
-    programs.alacritty = {
-      enable = true;
-      settings = {
-        window = {
-          padding.x = 15;
-          padding.y = 15;
-          decorations = "none";
-          startup_mode = "Windowed";
-          dynamic_title = true;
-          #opacity = 0.8;
-        };
-        cursor = {
-          style = {
-            shape = "Beam";
-            blinking = "On";
-          };
-        };
-        live_config_reload = true;
+lib.mkIf alacritty {
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window = {
+        padding.x = 10;
+        padding.y = 10;
+        decorations = "none";
+        startup_mode = "Windowed";
+        dynamic_title = true;
+        blur = true;
+        opacity = 0.9;
       };
+      window.dimensions = {
+        columns = 160;
+        lines = 80;
+      };
+      cursor = {
+        style = {
+          shape = "Beam";
+          blinking = "On";
+        };
+      };
+      env = {
+        term = "xterm-256color";
+      };
+      live_config_reload = true;
     };
-  }
+  };
+}
