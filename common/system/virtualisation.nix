6414dc4bf77f4bce7c4f986061147594d2ce1a71
Rishabh5321 <rishabh98818@outlook.com>
2024-08-28 21:58:54 +0530

    update virtualization

--- common/system/virtualisation.nix
+++ common/system/virtualisation.nix
@@ -1,14 +1,22 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemuOvmf = true;
+      qemuSwtpm = true;
+      qemuOvmfPackage = pkgs.OVMFFull;
+    };
+  };
+
+  environment.sessionVariables.LIBVIRT_DEFAULT_URI = ["qemu:///system"];
   environment.systemPackages = with pkgs; [
+    virt-manager
+    win-virtio
+    libvirt
     qemu
     swtpm
-    libvirt
   ];
-
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu;
-    };
+  users.users.putchar = {
+    extraGroups = ["libvirtd" "kvm" "qemu-libvirtd"];
   };
 }
